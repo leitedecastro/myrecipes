03df868f06e9d6f7ed6b0f65c42f2b183a674bce
Paulo Renato Castro <leitedecastro@gmail.com>
2017-10-07 05:21:02 +0000

    Add chefs destroy and tests

--- test/integration/chefs_test.rb
+++ test/integration/chefs_test.rb
@@ -5,6 +5,8 @@ class ChefsTestTest < ActionDispatch::IntegrationTest
   def setup
     @chef = Chef.create!(chefname: 'paulo', email: 'pr@example.com',
                          password: 'password', password_confirmation: 'password')
+    @chef2 = Chef.create!(chefname: 'renato', email: 'renato@example.com',
+                         password: 'password', password_confirmation: 'password')
   end
   
   test 'should get chefs index' do
@@ -18,4 +20,13 @@ class ChefsTestTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", chef_path(@chef), text: @chef.chefname.capitalize
   end
   
+  test 'should delete chef' do
+    get chefs_path
+    assert_template 'chefs/index'
+    assert_difference 'Chef.count', -1 do
+      delete chef_path(@chef2)
+    end
+    assert_redirected_to chefs_path
+    assert_not flash.empty?
+  end
 end
